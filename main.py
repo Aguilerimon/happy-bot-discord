90d3df5133ade379f63f540dffb11ff3f6f824f9
Antonio Aguilera <antonio.aguilerimon@gmail.com>
2021-03-17 15:38:52 -0600

    Cambio en el estado del bot en servidor

--- main.py
+++ main.py
@@ -41,10 +41,10 @@ async def on_ready():
 
 async def statuschange():
     while True:
-        await bot.change_presence(activity=discord.Game(name='con mi vagina'))
-        await asyncio.sleep(5)
-        await bot.change_presence(activity=discord.Game(name='happy ayudita'))
-        await asyncio.sleep(5)
+        await bot.change_presence(activity=discord.Game(name='Warframe'))
+        #await asyncio.sleep(5)
+        #await bot.change_presence(activity=discord.Game(name='happy ayudita'))
+        #await asyncio.sleep(5)
 
 
 def calculoprincipal(tier, quantity, percent):
